8b0f9be55b4452f79ae41d5cb6d4668fb64d489f
st0o0 <64534642+st0o0@users.noreply.github.com>
2025-12-12 15:21:47 +0100

    Add Props extensions

---
+++ src/Servus.Akka/PropsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Akka.Actor;
+using Akka.DependencyInjection;
+
+namespace Servus.Akka;
+
+public static class PropsExtensions
+{
+    public static Props Props<T>(this ActorSystem system, params object[] args)
+        where T : ActorBase => DependencyResolver.For(system).Props<T>(args);
+    
+    public static Props Props<T>(this IUntypedActorContext context, params object[] args)
+        where T: ActorBase => DependencyResolver.For(context.System).Props<T>(args);
+}
--- src/Servus.Akka/ResolveExtensions.cs
+++ src/Servus.Akka/ResolveExtensions.cs
@@ -5,41 +5,36 @@ namespace Servus.Akka;
 
 public static class ResolveExtensions
 {
-    private static IActorRef Resolve<TActor>(IActorRefFactory factory,
-        DependencyResolver resolver, string? name, params object[] args)
-        where TActor : ActorBase
-    {
-        var props = resolver.Props<TActor>(args);
-        return factory.ActorOf(props, name);
-    }
-    
+    private static IActorRef Resolve<TActor>(IActorRefFactory factory, Props props, string? name)
+        where TActor : ActorBase => factory.ActorOf(props, name);
+
     public static IActorRef ResolveChildActor<TActor>(this IActorContext context, string? name, params object[] args)
         where TActor : ActorBase
     {
-        var resolver = DependencyResolver.For(context.System);
-        return Resolve<TActor>(context, resolver, name, args);
+        var props = context.System.Props<TActor>(args);
+        return Resolve<TActor>(context, props, name);
     }
 
     public static IActorRef ResolveChildActor<TActor>(this IActorContext context, params object[] args)
         where TActor : ActorBase
         => ResolveChildActor<TActor>(context, null, args);
-    
+
     public static IActorRef ResolveActor<TActor>(this IActorContext context, string? name, params object[] args)
         where TActor : ActorBase
         => context.System.ResolveActor<TActor>(name, args);
-    
+
     public static IActorRef ResolveActor<TActor>(this IActorContext context, params object[] args)
         where TActor : ActorBase
         => context.System.ResolveActor<TActor>(args);
-    
+
     public static IActorRef ResolveActor<TActor>(this ActorSystem system, params object[] args)
         where TActor : ActorBase
         => ResolveActor<TActor>(system, null, args);
-    
+
     public static IActorRef ResolveActor<TActor>(this ActorSystem system, string? name, params object[] args)
         where TActor : ActorBase
     {
-        var resolver = DependencyResolver.For(system);
-        return Resolve<TActor>(system, resolver, name, args);
+        var props = system.Props<TActor>(args);
+        return Resolve<TActor>(system, props, name);
     }
 }
